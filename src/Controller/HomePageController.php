f6b0ac8d0663aefb6c59633527d035906b9ee2ec
Mehul Joshi <mehuljoshi1310@gmail.com>
2025-03-11 16:06:11 +0530

    Add redirection to homepagecontroller

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
+    #[Route('/', name: 'homepage_redirect')]
+    public function redirectToHome(): RedirectResponse
+    {
+        return $this->redirectToRoute('home');
+    }
+
     #[Route('/home', name: 'home')]
     public function home(): Response
     {
