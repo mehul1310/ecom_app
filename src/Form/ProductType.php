1778f9ae9a09ce57286de8905d5c58cda51bf6e2
Mehul Joshi <mehuljoshi1310@gmail.com>
2025-03-11 16:00:28 +0530

    Removed unused use statement from producttype form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,7 +6,6 @@ use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
